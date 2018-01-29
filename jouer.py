860ca91d35bc64fc02b8fafa1ac4de18161b7b37
Angelo Ortiz <3775902@ppti-14-303-07.ufr-info-p6.jussieu.fr>
2018-01-29 19:48:12 +0100

    Fonceur et defenseur

--- jouer.py
+++ jouer.py
@@ -4,9 +4,11 @@ from strategies import *
 pyteam = SoccerTeam(name="PyTeam")
 thon = SoccerTeam(name="ThonTeam")
 #pyteam.add("PyPlayer",Strategy()) #Strategie qui ne fait rien
-pyteam.add("PyPlayer",FonceurStrategy()) #Strategie qui ne fait rien
-#pyteam.add("PyPlayer",DribblerStrategy()) #Strategie qui ne fait rien
-thon.add("ThonPlayer",Strategy())   #Strategie qui ne fait rien
+#pyteam.add("PyPlayer",FonceurStrategy()) #Strategie qui ne fait rien
+pyteam.add("DribbleurPyPlayer1",DribblerStrategy()) #Strategie qui ne fait rien
+#pyteam.add("DribbleurPyPlayer2",DefendreStrategy()) #Strategie qui ne fait rien
+#thon.add("DribbleurThonPlayer1",DribblerStrategy())   #Strategie qui ne fait rien
+#thon.add("GardienThonPlayer2",DefendreStrategy())   #Strategie qui ne fait rien
 #thon.add("ThonPlayer",FonceurStrategy())   #Strategie aleatoire
 #thon.add("ThonPlayer",DribblerStrategy())   #Strategie qui ne fait rien
 
--- strategies.py
+++ strategies.py
@@ -3,6 +3,42 @@ from soccersimulator import SoccerTeam, Simulation
 from soccersimulator import show_simu
 from soccersimulator.settings import *
 
+## StateFoot ...
+class StateFoot(object):
+    def __init__(self,state,id_team,id_player):
+        self.state = state
+        self.key = (id_team,id_player)
+    def my_position(self):
+        return self.state.player_state(*self.key).position
+    def ball_position(self):
+        return self.state.ball.position
+    def aller_vers_balle(self):
+        p = Vector2D()
+        return self.aller(p)
+    def aller(self,p):
+        return SoccerAction(p-self.my_position())
+    def shoot(self,p):
+        return SoccerAction(Vector2D(),(p-self.ball_position()).norm_max(3.5))
+    def distance_ball(self,p):
+        return (self.ball_position()-p).norm
+    def distance_ball_joueur(self):
+        return self.distance_ball(self.my_position())
+    def est_plus_proche(self,p):
+        dist_ball_p = self.distance_ball(p)
+        return self.distance_ball_joueur() < dist_ball_p
+    def adversaire_1v1(self):
+        return self.state.player_state(3-self.key[0],0)
+    def can_shoot(self):
+        dist_ball_joueur = self.distance_ball(self.my_position())
+        ball_est_proche = dist_ball_joueur <= PLAYER_RADIUS + BALL_RADIUS
+        return ball_est_proche and self.state.player_state(*self.key).can_shoot()
+    def cage_but(self):
+        return Vector2D(0. if self.key[0] == 2 else GAME_WIDTH,GAME_HEIGHT/2.)
+    def ma_cage(self):
+        return Vector2D(0. if self.key[0] == 1 else GAME_WIDTH,GAME_HEIGHT/2.)
+    def compute_strategy(self):
+        #TODO
+        return self.aller(p)
 
 ## Strategie aleatoire
 class RandomStrategy(Strategy):
@@ -16,17 +52,10 @@ class FonceurStrategy(Strategy):
     def __init__(self):
         Strategy.__init__(self,"Fonceur")
     def compute_strategy(self,state,id_team,id_player):
-        player_state = state.player_state(id_team,id_player)
-        player_position = state.player_state(id_team,id_player).position
-        ecart_player_ball = state.ball.position - player_position
-        dist_player_ball = ecart_player_ball.norm
-        pos_x = 0. if id_team == 2 else GAME_WIDTH
-        goal_milieu = Vector2D(pos_x,GAME_HEIGHT/2.)
-        if dist_player_ball <= PLAYER_RADIUS+BALL_RADIUS and player_state.can_shoot():
-            acceleration = goal_milieu - state.ball.position
-            return SoccerAction(acceleration,acceleration.copy().norm_max(maxPlayerShoot))
-        ecart_player_ball.norm_max(maxPlayerAcceleration)
-        return SoccerAction(ecart_player_ball)
+        myState = StateFoot(state,id_team,id_player)
+        if myState.can_shoot():
+            return myState.shoot(myState.cage_but())
+        return myState.aller(myState.ball_position())
 
 ## Strategie Dribbler
 class DribblerStrategy(Strategy):
@@ -43,3 +72,15 @@ class DribblerStrategy(Strategy):
             return SoccerAction(acceleration,acceleration.copy().norm_max(maxPlayerShoot/2.))
         ecart_player_ball.norm = maxPlayerAcceleration
         return SoccerAction(ecart_player_ball)
+
+## Strategie Defendre
+class DefendreStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Defendre")
+    def compute_strategy(self,state,id_team,id_player):
+        myState = StateFoot(state,id_team,id_player)
+        if myState.can_shoot():
+            return myState.shoot(myState.cage_but())
+        if myState.est_plus_proche(myState.adversaire_1v1().position):
+            return myState.aller(myState.ball_position())
+        return myState.aller(myState.ma_cage())
