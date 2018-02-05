818651a3695805c9b3d2a41bcb98edb10979228b
Angelo Ortiz <3775902@ppti-14-303-06.ufr-info-p6.jussieu.fr>
2018-02-05 16:11:56 +0100

    Erreur

--- exemple_challenge.py
+++
@@ -1,9 +0,0 @@
-from soccersimulator import ChallengeFonceurButeur, SoccerTeam,show_simu
-from strategies import RandomStrategy, FonceurStrategy
-
-team = SoccerTeam("FonceurEquipe")
-team.add("FonceurJoueur",FonceurStrategy())
-
-challenge = ChallengeFonceurButeur(team,max_but=20)
-show_simu(challenge)
-print("temps moyen : ",challenge.stats_score, "\nliste des temps",challenge.resultats)
