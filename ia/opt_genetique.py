d22dac953eb36dff4e2c9304ffd83f3b56c26cde
Angelo Ortiz <angelo@AngeloPC.localdomain>
2018-02-27 00:00:17 +0100

    Amelioration de l'algo gene

--- ia/opt_genetique.py
+++ ia/opt_genetique.py
@@ -1,46 +1,123 @@
 # coding: utf-8
 from __future__ import print_function, division
-from soccersimulator import SoccerTeam, Simulation, Strategy, show_simu, Vector2D
-from soccersimulator.settings import GAME_WIDTH, GAME_HEIGHT
-from ia.strategies import FonceurStrategy, FonceurChallenge1Strategy
-from ia.tools import StateFoot
-from ia.conditions import can_shoot
+from soccersimulator import SoccerTeam
+from ia.strategies import FonceurStrategy, GardienStrategy, AttaquantStrategy
 import random
 
+def setCounters(simu, team1, team2):
+    """
+        Met a jour les compteurs des deux vecteurs qui 
+        ont participe au dernier match avec les regles 
+        connues du football (buts et points)
+    """
+    team1.fg = simu.get_score_team(1)
+    team2.fg = simu.get_score_team(2)
+    team1.ag, team2.ag = team2.fg, team1.fg
+    if team1.fg > team2.fg:
+        team1.pts += 3
+    elif team1.fg == team2.fg:
+        team1.pts += 1
+        team2.pts += 1
+    else:
+        team2.pts += 3
+
+def getDistinctTuple(low=0, high):
+    """
+    Renvoie un couple d'entiers distincts compris entre 
+    low (inclus) et high (exlu)
+    """
+    i = random.randrange(low, high)
+    while True:
+        j = random.randrange(low, high)
+        if i != j: break
+    return i, j
+
+@total_ordering
 class dictParams(object):
     def __init__(self):
-        self.params = {"alpha": 0., "beta":0., "power":0.,"tempsI":0, "angle";0.}
+        self.params = {"alpha": 0., "beta":0., "power":0.,"tempsI":0, "angle":0.}
+        self.pts = 0 # le nombre de points obtenus (V,N,D) = (3,1,0)
+        self.fg = 0     # le nombre de buts marques
+        self.ag = 0     # le nombre de buts encaisses
+
+    def __lt__(self, ther):
+        return ((self.pts, self.fg - self.ag, self.fg) < \
+                (other.pts, other.fg - other.ag, other.fg))
+    
+    def __eq__(self, ther):
+        return ((self.pts, self.fg - self.ag, self.fg) == \
+                (other.pts, other.fg - other.ag, other.fg))
 
     @classmethod
     def limits(cls):
+        """
+            Renvoie un dictionnaire avec les bornes de chaque 
+            parametre
+        """
         return {"alpha": (0.,0.), "beta":(0.,0.), "power":(0.,0.),"tempsI":(0.,0.), \
-                "angle";(0.,0.)}
+                "angle":(0.,0.)}
     
     def random(self, parameters):
+        """
+            Randomise tous les parametres du vecteur avec des 
+            valeurs comprises dans les limites acceptables
+        """
         limits = dictParams.limits()
         for p in parameters:
             pLimits = limits[p]
             self.params[p] = random.uniform(pLimits[0], pLimits[1])
 
     def isValid(self, p):
+        """
+            Renvoie vrai ssi la valeur du parametre p est 
+            bien valide, ie comprise dans ses limites definies
+        """
         val = self.params[p]
         limits = dictParams.limits()
         return val >= limits[p][0] and val <= limits[p][1]
+
+    def restart(self):
+        """
+            Remet a zero tous les compteurs du vecteur, 
+            a savoir, le nombre de points, le nombre de buts 
+            marques et le nombre de buts encaisses
+        """
+        self.pts = 0
+        self.fg = 0
+        self.ag = 0
         
 class GKStrikerTeam(object):
-    def __init__(self, size=10):
+    def __init__(self, size=20, keep=0.4, coProb=0.7, mProb=0.08):
         self.team = None
-        self.gk = GardienStrategy()
-        self.st = FonceurStrategy()
-        self.params = [dictParams()]*size
-        self.gk_params = ["tempsI"]
-        self.st_params = ["alpha", "beta", "angle"]
+        self.keep = keep # pourcentage de conservation
+        self.coProb = coProb # probabilite de croisement
+        self.mProb = mProb # probabilite de mutation
+        self.gk = GardienStrategy() # strategie goalkeeper (gk)
+        self.st = FonceurStrategy() # strategie striker (st)
+        self.params = [dictParams()]*size # vecteurs de parametres
+        self.gk_params = ["tempsI"] # parametres du gk
+        self.st_params = ["alpha", "beta", "angle"] # parametres du st
 
-    def initialize(self):
+    def random(self):
+        """
+            Randomise tous les vecteurs
+        """
+        pList = self.gk_params + self.st_params
         for p in self.params:
-            p.random(self.gk_params + self.st_params)
+            p.random(pList)
 
-    def get_team(self, i):
+    def restart(self):
+        """
+            Remet a zero tous les compteurs de chaque vecteur
+        """
+        for p in self.params:
+            p.restart()
+
+    def getTeam(self, i):
+        """
+            Renvoie l'equipe composee des strategies contenues 
+            dans l'instance avec l'i-ieme vecteur de parametres
+        """
         params = self.params[i]
         for p in self.gk_params:
             self.gk.dictGK[p] = params[p]
@@ -51,23 +128,30 @@ class GKStrikerTeam(object):
         self.team.add(self.st.name, self.st)
         return self.team
 
-    def crossover(self, i, j):
+    def getVector(self, i):
         """
-        Proabiblite de faire un crossover ; 0.7
+            Renvoie l'i-ieme vecteur de parametres
         """
-        p1 = self.params[i]
-        p2 = self.params[j]
-        p1new = p1.deepcopy()
-        p2new = p2.deepcopy()
+        return self.params[i]
+
+    def crossover(self, i, j, k):
+        """
+            Fait un croisement entre les vecteurs i- et j-ieme 
+            et le met dans le k-ieme vecteur
+        """
+        pi = self.params[i]
+        pj = self.params[j]
+        pk = pi.deepcopy()
         pList = self.gk_params + self.st_params
         index = random.randrange(0, len(pList))
-        for i in range(index):
-            p1new.params[pList[i]] = p2.params[pList[i]]
-            p2new.params[pList[i]] = p1.params[pList[i]]
-        self.params[i] = p1new
-        self.params[j] = p2new
+        for l in range(index):
+            pk.params[pList[l]] = pj.params[pList[l]]
 
     def addNoise(self, i, p):
+        """
+            Ajoute du bruit au parametre p de l'i-ieme vecteur 
+            de parametres
+        """
         val = self.params[i].params[p]
         while True:
             valNoise = val * random.uniform(0.9,1.1)
@@ -76,89 +160,33 @@ class GKStrikerTeam(object):
             if self.params[i].isValid(p):
                 break
     
-    def mutation(self, i, j):
+    def mutation(self, i, j, k):
         """
-        Proabiblite de faire une mutation ; 0.01 - 0.1 (a definir)
+            Fait une mutation entre les vecteurs i- et j-ieme 
+            dans le vecteur k-ieme, ie un croisement avec du 
+            bruit sur l'un des parametres
         """
-        self.crossover(i, j)
+        self.crossover(i, j, k)
         pList = self.gk_params + self.st_params
-        pi = random.randrange(0, len(pList))
-        self.addNoise(index, i, pList[pi])
-        pj = random.randrange(0, len(pList))
-        self.addNoise(index, j, pList[pj])
-        
-    def begin_match(self, team1, team2, state):
-        self.last = 0  # Step of the last round
-        self.crit = 0  # Criterion to maximize (here, number of goals)
-        self.cpt = 0  # Counter for trials
-        self.param_keys = list(self.params.keys())  # Name of all parameters
-        self.param_id = [0] * len(self.params)  # Index of the parameter values
-        self.param_id_id = len(self.params) - 1  # Index of the current parameter
-        self.res = dict()  # Dictionary of results
+        pl = random.randrange(0, len(pList))
+        self.addNoise(self, k, pList[pl])
 
-    def begin_round(self, team1, team2, state):
-        dist = self.params['dist'][0]
-        ball = Vector2D.create_random(low=-1, high=1)
-        if ball.x < 0. : ball.x = -ball.x 
-        aleat = Vector2D.create_random(low=15, high=dist)
-        ball.normalize().scale(aleat.x)
-        ball.y += GAME_HEIGHT/2.
-    
-        # Player and ball postion (random)
-        self.simu.state.states[(2, 0)].position = ball.copy()  # Shooter position
-        self.simu.state.states[(2, 0)].vitesse = Vector2D()  # Shooter acceleration
-        self.simu.state.ball.position = ball.copy()  # Ball position
-
-        # Last step of the game
-        self.last = self.simu.step
-
-        # Set the current value for the current parameter
-        for i, (key, values) in zip(self.param_id, self.params.items()):
-            setattr(self.strategy, key, values[i])
-
-    def update_round(self, team1, team2, state):
-        # Stop the round if it is too long
-        if state.step > self.last + self.max_round_step:
-            self.simu.end_round()
-
-    def end_round(self, team1, team2, state):
-        # A round ends when there is a goal
-        if state.goal > 0:
-            self.crit += 1  # Increment criterion
-
-        self.cpt += 1  # Increment number of trials
-        if self.cpt >= self.trials:
-            # Save the result
-            res_key = tuple()
-            for i, values in zip(self.param_id, self.params.values()):
-                res_key += values[i],
-            self.res[res_key] = self.crit * 1. / self.trials
-            print(res_key, self.crit)
-
-            # Reset parameters
-            self.crit = 0
-            self.cpt = 0
-
-            # Go to the next parameter value to try
-            key3 = self.param_keys[self.param_id_id]
-            key2 = self.param_keys[self.param_id_id-1]
-            key1 = self.param_keys[self.param_id_id-2]
-            if self.param_id[self.param_id_id] < len(self.params[key3]) - 1:
-                self.param_id[self.param_id_id] += 1
-            elif self.param_id[self.param_id_id-1] < len(self.params[key2]) - 1:
-                self.param_id[self.param_id_id] = 0
-                self.param_id[self.param_id_id-1] += 1
-            elif self.param_id[self.param_id_id-2] < len(self.params[key1]) - 1:
-                self.param_id[self.param_id_id] = 0
-                self.param_id[self.param_id_id-1] = 0
-                self.param_id[self.param_id_id-2] += 1
-                self.param_id_id += 1
-            else:
-                self.simu.end_match()
-
-        for i, (key, values) in zip(self.param_id, self.params.items()):
-            print("{}: {}".format(key, values[i]), end="   ")
-        print("Crit: {}   Cpt: {}".format(self.crit, self.cpt))
-
-    def get_res(self):
-        return self.res
+    def update(self):
+        """
+            Garde les meilleurs resultats, qui representent 
+            le keep*100% superieur, et modifie le reste avec 
+            soit une mutation, soit un croisement des meilleurs
+            scores
+        """
+        self.params.sort()
+        size = len(self.params)
+        nKeep = int(size * self.keep)
+        for k in range(nKeep, size):
+            #TODO la, on ne modifie pas tous les mauvais resultats,
+            # il faut utiliser une boucle infinie avec break lorsque 
+            # le mauvais resultat a disparu
+            i, j = getDistinctTuple(high=nKeep)
+            if random.random() < self.mProb:
+                self.mutation(i, j, k)
+            elif random.random() < self.coProb:
+                self.crossover(i, j, k)
--- test_genetique.py
+++ test_genetique.py
@@ -1,14 +1,26 @@
 from soccersimulator import SoccerTeam, Simulation
 from soccersimulator import show_simu
-from ia.strategies import *
+from ia.opt_generique import setCounters, dictParams, GKStrikerTeam, GKCForwardTeam
 
-gk_st_team = GKStrikerTeam()
-gk_cf_team = GKCForwardTeam()
-for i in range(10000):
-    lteam = gk_st_team.get_team(j)
-    rteam = gk_cf_team.get_team(k)
-    simu = Simulation(lteam,rteam)
-   # TODO
+size = 30
+gk_st_team = GKStrikerTeam(size)
+gk_st_team.random()
+gk_cf_team = GKCForwardTeam(size)
+gk_cf_team.random()
+for i in range(size):
+    gk_st = gk_st_team.getTeam(i)
+    gk_st_p = gk_st_team.getVector(i)
+    for j in range(size):
+        gk_cf = gk_cf_team.getTeam(j)
+        gk_cf_p = gk_cf_team.getVector(j)
+        team = {1: (gk_st, gk_st_p), 2:(gk_cf, gk_cf_p)}
+        if random.random() < 0.5:
+            team[1], team[2] = team[2], team[1]
+        simu = Simulation(team[1][0],team[2][0])
+        setCounters(simu, team[1][1], team[2][1])
+
+for i in range(1000):
+    
    """
     1/ lancer d'abord les size*size matches (10*10 ?)
     2/ reprendre les x meilleurs resultats (25% ?)
