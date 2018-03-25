c94d898b650e518ba127631a14ee6e89c50c5d80
Angelo ORTIZ TANDAZO <angortizt@gmail.com>
2018-03-25 21:35:34 +0200

    Fin S8

--- ia/__init__.py
+++ ia/__init__.py
@@ -7,14 +7,14 @@ def get_team(nb_players):
     if nb_players == 1:
         myteam.add("  9_Fonceur", FonceurStrategy())
     if nb_players == 2:
-        myteam.add("  7_Attaquant", AttaquantModifStrategy(fn_st="st_dico_FS7_OK.pkl"))
-        myteam.add("  1_Goal", GardienModifStrategy(fn_gk="gk_dico_FS7_OK.pkl",fn_st="st_dico_FS7_OK.pkl"))
+        myteam.add("  7_Attaquant", AttaquantModifStrategy(fn_gk="gk_dico_0325_p5_short.pkl", fn_st="st_dico_0325_p5_short.pkl"))
+        myteam.add("  1_Goal", GardienModifStrategy(fn_gk="gk_dico_0325_p5_short.pkl",fn_st="st_dico_0325_p5_short.pkl"))
     if nb_players == 4:
         myteam.add("Joueur 1",GardienStrategy())
         myteam.add("Joueur 2",RandomStrategy())
         myteam.add("Joueur 3",RandomStrategy())
         myteam.add("Joueur 4",RandomStrategy())
-    return myteam	
+    return myteam
 
 def get_team_challenge(num):
 	myteam = SoccerTeam(name="ChPerFusion")
